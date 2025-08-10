074fec14eae88b5ff228293b65c7d68f068a23af
CarlosOrte <al168703@alumnos.uacj.mx>
2025-08-10 15:54:11 -0600

    AWS_S3_VERIFY = True

--- portfolio_backend/settings.py
+++ portfolio_backend/settings.py
@@ -81,7 +81,7 @@ AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 # AWS_DEFAULT_ACL = 'public-read'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
+AWS_S3_VERIFY = True
 # --------------------
 # 5. OTRAS CONFIGURACIONES
 # --------------------
