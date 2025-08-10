c11b3f9e5e8d909628c0cbd2b036f7d44bd4e6cf
CarlosOrte <al168703@alumnos.uacj.mx>
2025-08-10 15:24:58 -0600

    se comentó la linea AWS_DEFAULT_ACL = 'public-read'

--- portfolio_backend/settings.py
+++ portfolio_backend/settings.py
@@ -78,7 +78,7 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = 'us-east-2' # Valor corregido
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-AWS_DEFAULT_ACL = 'public-read'
+# AWS_DEFAULT_ACL = 'public-read'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
